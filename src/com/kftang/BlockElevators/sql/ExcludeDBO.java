2f0b8702d4b337db3958b53471cd7d827208e573
Kenny Tang <kennyftang@gmail.com>
2017-04-02 13:06:29 -0500

    Added basic DBO

--- src/com/kftang/BlockElevators/sql/ExcludeDBO.java
+++ src/com/kftang/BlockElevators/sql/ExcludeDBO.java
@@ -1,7 +1,33 @@
 package com.kftang.BlockElevators.sql;
 
+import org.bukkit.Location;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Kenny Tang (c) 2017
  */
 public class ExcludeDBO {
+    private Connection connection;
+
+    ExcludeDBO() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        if(!new File("plugins/BlockElevators/exclude.sqlite").exists()) {
+            connection = DriverManager.getConnection("jdbc:sqlite:plugins/BlockElevators/exclude.sqlite");
+            connection.createStatement().executeQuery("");
+        } else {
+            connection = DriverManager.getConnection("jdbc:sqlite:plugins/BlockElevators/exclude.sqlite");
+        }
+    }
+
+    boolean isExcluded(Location location) {
+        return false;
+    }
+
+    boolean addExcludedBlock(Location location) {
+        return true;
+    }
 }
